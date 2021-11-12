9abe8fca711356d845da4c38591f1297c6805286
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-11-11 17:10:20 -0700

    docs(examples): Add requirements.txt to each

--- examples/howdoi/cli.py
+++ examples/howdoi/cli.py
@@ -3,5 +3,5 @@ from trame import get_cli_parser
 if __name__ == "__main__":
     parser = get_cli_parser()
     parser.add_argument("-o", "--output", help="Working directory")
-    args = parser.parse_known_args()
+    (args, _unknown) = parser.parse_known_args()
     print(args.output)

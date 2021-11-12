c1005264356ccfcdf8204d7a3018633717d3da31
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-11-12 14:42:11 -0700

    fix(version): Improve version handling

--- trame/utils/version.py
+++ trame/utils/version.py
@@ -1,10 +1,19 @@
-# When we require python 3.8, we can use importlib.metadata instead...
-from pkg_resources import get_distribution, DistributionNotFound
+try:
+    # Use importlib metadata if available
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # No importlib metadata. Try to use pkg_resources instead.
+    from pkg_resources import (
+        get_distribution,
+        DistributionNotFound as PackageNotFoundError,
+    )
+    def version(x):
+        return get_distribution(x).version
 
 
 def get_version():
     try:
-        return get_distribution("trame").version
-    except DistributionNotFound:
+        return version("trame")
+    except PackageNotFoundError:
         # package is not installed
         pass

ae054c8d797b68b24f809e08e94b935e1c835a5e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-03-03 17:11:42 -0700

    docs(example): add www generator example

---
+++ examples/www/README.md
@@ -0,0 +1,5 @@
+Dummy app that can be use to generate common static trame website for serving it on apache/nginx.
+
+```
+python ./examples/www/app.py --www trame:./examples/www/static/
+```
---
+++ examples/www/app.py
@@ -0,0 +1,23 @@
+from trame.layouts import FullScreenPage
+
+# Bring all web dependencies
+from trame.html import (
+    deckgl, # noqa
+    markdown, # noqa
+    matplotlib, # noqa
+    observer, # noqa
+    paraview, # noqa
+    plotly, # noqa
+    router, # noqa
+    # simput, # noqa - (pip install simput)
+    vega, # noqa
+    vtk, # noqa
+    vuetify, # noqa
+    widgets, # noqa
+    xai, # noqa
+)
+
+layout = FullScreenPage("Empty")
+
+if __name__ == "__main__":
+    layout.start()

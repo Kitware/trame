9f3d0df6b3e069257278e0fae29e994ca65bfc03
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-15 17:32:41 -0600

    docs(v2): howdoi/interactive

--- examples/howdoi/interactive.py
+++ examples/howdoi/interactive.py
@@ -1,39 +1,48 @@
-from trame import state
-from trame.layouts import SinglePage
-from trame.html import Div, vuetify
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import html, vuetify
 
+server = get_server()
+state = server.state
 
-# Model
-initial_number = 5
-
+# Initial state values
+DEFAULT_VALUE = 5
+state.trame_title = "Counter"
 
 # Updates
 def increment():
-    state.myNumber += 1
+    state.my_number += 1
 
 
 def decrement():
-    state.myNumber -= 1
+    state.my_number -= 1
 
 
-@state.change("myNumber")
-def validateMyNumber(myNumber, **kwargs):
+@state.change("my_number")
+def validate_my_number(my_number, **kwargs):
+    if isinstance(my_number, int):
+        # Prevent infinit loop
+        return
+
     try:
-        state.myNumber = int(myNumber)
+        state.my_number = int(my_number)
     except:
-        state.myNumber = initial_number
+        state.my_number = DEFAULT_VALUE
 
 
-layout = SinglePage("Counter")
-layout.title.set_text("Simple Counter Demo")
+with SinglePageLayout(server) as layout:
+    layout.title.set_text("Simple Counter Demo")
 
-with layout.content:
-    with Div(classes="ma-8"):
-        vuetify.VBtn("Increment", click=increment)
-        vuetify.VTextField(v_model=("myNumber", initial_number))
-        vuetify.VBtn("Decrement", click=decrement)
-        vuetify.VSlider(v_model=("myNumber",))
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VBtn("-", click=decrement)
+        vuetify.VBtn("+", click=increment)
+
+    with layout.content:
+        with html.Div(classes="ma-8"):
+            vuetify.VTextField(v_model=("my_number", DEFAULT_VALUE))
+            vuetify.VSlider(v_model=("my_number",))
 
 
 if __name__ == "__main__":
-    layout.start()
+    server.start()

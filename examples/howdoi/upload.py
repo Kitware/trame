faf00108b8d4cb3f4c4babc256ee995e181bfa34
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-15 17:33:41 -0600

    docs(v2): howdoi/upload

--- examples/howdoi/upload.py
+++ examples/howdoi/upload.py
@@ -1,24 +1,29 @@
-from trame import change
-from trame.layouts import FullScreenPage
-from trame.html import vuetify
+from trame.app import get_server
+from trame.ui.vuetify import VAppLayout
+from trame.widgets import vuetify
 
-layout = FullScreenPage("File upload")
-layout.children += [
+server = get_server(log_network="./upload-exchange.txt")
+state = server.state
+
+with VAppLayout(server):
     vuetify.VFileInput(
         v_model=("file_exchange", None),
     )
-]
 
 
-@change("file_exchange")
+@state.change("file_exchange")
 def file_uploaded(file_exchange, **kwargs):
+    if file_exchange is None:
+        return
+
     file_name = file_exchange.get("name")
     file_size = file_exchange.get("size")
     file_time = file_exchange.get("lastModified")
     file_mime_type = file_exchange.get("type")
     file_binary_content = file_exchange.get("content")
-    print(f"Go file {file_name} of size {file_size}")
+    print(f"Got file {file_name} of size {file_size}")
+    print(file_binary_content)
 
 
 if __name__ == "__main__":
-    layout.start()
+    server.start()

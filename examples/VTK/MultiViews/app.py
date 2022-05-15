ba381b3ad286b6269e181d76d1e13ef0cc2d5bc7
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-15 17:58:11 -0600

    docs(v2): VTK/MultiViews

--- examples/VTK/MultiViews/app.py
+++ examples/VTK/MultiViews/app.py
@@ -1,8 +1,6 @@
-import asyncio
-import enum
-from trame import state, controller as ctrl
-from trame.html import vuetify, vtk
-from trame.layouts import SinglePage
+from trame.app import get_server
+from trame.widgets import vuetify, vtk
+from trame.ui.vuetify import SinglePageLayout
 
 from vtkmodules.vtkFiltersSources import vtkConeSource
 from vtkmodules.vtkRenderingCore import (
@@ -16,6 +14,15 @@ from vtkmodules.vtkRenderingCore import (
 # VTK factory initialization
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleSwitch  # noqa
 import vtkmodules.vtkRenderingOpenGL2  # noqa
+
+# -----------------------------------------------------------------------------
+# Trame initialization
+# -----------------------------------------------------------------------------
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+state.trame__title = "VTK Remote rendering"
 
 # -----------------------------------------------------------------------------
 # VTK code
@@ -83,45 +90,44 @@ def update_reset_resolution():
 # GUI
 # -----------------------------------------------------------------------------
 
-layout = SinglePage("VTK Remote rendering", on_ready=update_cone)
-layout.logo.click = ctrl.reset_camera
-layout.title.set_text("Cone Application")
+with SinglePageLayout(server) as layout:
+    layout.icon.click = ctrl.reset_camera
+    layout.title.set_text("Cone Application")
 
-with layout.toolbar:
-    vuetify.VSpacer()
-    vuetify.VSlider(
-        v_model=("resolution", DEFAULT_RESOLUTION),
-        min=3,
-        max=60,
-        step=1,
-        hide_details=True,
-        dense=True,
-        style="max-width: 300px",
-    )
-    vuetify.VDivider(vertical=True, classes="mx-2")
-    with vuetify.VBtn(icon=True, click=update_reset_resolution):
-        vuetify.VIcon("mdi-undo-variant")
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VSlider(
+            v_model=("resolution", DEFAULT_RESOLUTION),
+            min=3,
+            max=60,
+            step=1,
+            hide_details=True,
+            dense=True,
+            style="max-width: 300px",
+        )
+        vuetify.VDivider(vertical=True, classes="mx-2")
+        with vuetify.VBtn(icon=True, click=update_reset_resolution):
+            vuetify.VIcon("mdi-undo-variant")
 
-
-with layout.content:
-    with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
-        container = None
-        for idx, render_window in enumerate(render_windows):
-            if idx % NB_COLS == 0:
-                container = vuetify.VRow(
-                    classes="pa-0 ma-0",
-                    style=f"height: {NB_COLS * 100 / len(render_windows)}%;",
-                )
-            with container:
-                with vuetify.VCol(classes="pa-0 ma-0"):
-                    view = vtk.VtkRemoteView(render_window, ref=f"view{idx}")
-                    html_views.append(view)
-                    ctrl.update_views.add(view.update)
-                    ctrl.reset_camera.add(view.reset_camera)
+    with layout.content:
+        with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
+            container = None
+            for idx, render_window in enumerate(render_windows):
+                if idx % NB_COLS == 0:
+                    container = vuetify.VRow(
+                        classes="pa-0 ma-0",
+                        style=f"height: {NB_COLS * 100 / len(render_windows)}%;",
+                    )
+                with container:
+                    with vuetify.VCol(classes="pa-0 ma-0"):
+                        view = vtk.VtkRemoteView(render_window, ref=f"view{idx}")
+                        html_views.append(view)
+                        ctrl.update_views.add(view.update)
+                        ctrl.reset_camera.add(view.reset_camera)
 
 # -----------------------------------------------------------------------------
 # Main
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    layout.start()
+    server.start()

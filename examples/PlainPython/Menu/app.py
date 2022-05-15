f9582202f7ee08434b5a7e53f375b1041bee6dfe
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-15 17:43:46 -0600

    docs(v2): PlainPython/Menu

--- examples/PlainPython/Menu/app.py
+++ examples/PlainPython/Menu/app.py
@@ -1,8 +1,13 @@
-from trame import state
-from trame.html import vuetify
-from trame.layouts import SinglePage
+from trame.app import get_server
+from trame.widgets import vuetify
+from trame.ui.vuetify import SinglePageLayout
 
-layout = SinglePage("Menu example")
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+server = get_server()
+state, ctrl = server.state, server.controller
 
 state.menu_items = ["one", "two", "three"]
 
@@ -11,21 +16,29 @@ def print_item(item):
     print("Clicked on", item)
 
 
-with layout.toolbar:
-    vuetify.VSpacer()
-    with vuetify.VMenu():
-        with vuetify.Template(v_slot_activator="{ on, attrs }"):
-            with vuetify.VBtn(icon=True, v_bind="attrs", v_on="on", v_on_click="test"):
-                vuetify.VIcon("mdi-dots-vertical")
-        with vuetify.VList(), vuetify.VListItem(
-            v_for="(item, i) in menu_items",
-            key="i",
-            value=["item"],
-        ):
-            vuetify.VBtn(
-                "{{ item }}",
-                click=(print_item, "[item]"),
-            )
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+state.trame__title = "Menu example"
+
+with SinglePageLayout(server) as layout:
+    with layout.toolbar:
+        vuetify.VSpacer()
+        with vuetify.VMenu():
+            with vuetify.Template(v_slot_activator="{ on, attrs }"):
+                with vuetify.VBtn(icon=True, v_bind="attrs", v_on="on"):
+                    vuetify.VIcon("mdi-dots-vertical")
+            with vuetify.VList():
+                with vuetify.VListItem(
+                    v_for="(item, i) in menu_items",
+                    key="i",
+                    value=["item"],
+                ):
+                    vuetify.VBtn(
+                        "{{ item }}",
+                        click=(print_item, "[item]"),
+                    )
 
 if __name__ == "__main__":
-    layout.start()
+    server.start()

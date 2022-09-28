af70938509d966eae4cea5d76389ecdd9c97bc88
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-09-27 18:56:04 -0600

    docs(download): Add binary download example

---
+++ examples/validation/core/24_vtk_download_image.py
@@ -0,0 +1,98 @@
+from trame.app import get_server
+from trame.widgets import vuetify, vtk
+from trame.ui.vuetify import SinglePageLayout
+
+# -----------------------------------------------------------------------------
+# VTK
+# -----------------------------------------------------------------------------
+
+from vtkmodules.vtkFiltersSources import vtkConeSource
+from vtkmodules.vtkIOImage import vtkPNGWriter
+from vtkmodules.vtkRenderingCore import (
+    vtkRenderer,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkPolyDataMapper,
+    vtkActor,
+    vtkWindowToImageFilter,
+)
+import vtkmodules.vtkRenderingOpenGL2  # noqa
+from vtkmodules.vtkInteractionStyle import vtkInteractorStyleSwitch  # noqa
+
+DEFAULT_RESOLUTION = 6
+
+renderer = vtkRenderer()
+renderWindow = vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+
+renderWindowInteractor = vtkRenderWindowInteractor()
+renderWindowInteractor.SetRenderWindow(renderWindow)
+renderWindowInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
+
+cone_source = vtkConeSource()
+mapper = vtkPolyDataMapper()
+actor = vtkActor()
+mapper.SetInputConnection(cone_source.GetOutputPort())
+actor.SetMapper(mapper)
+renderer.AddActor(actor)
+renderer.ResetCamera()
+renderWindow.Render()
+
+# -----------------------------------------------------------------------------
+# Web App setup
+# -----------------------------------------------------------------------------
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+
+@state.change("resolution")
+def update_resolution(resolution, **kwargs):
+    cone_source.SetResolution(int(resolution))
+    ctrl.view_update()
+
+
+@ctrl.trigger("download_screenshot")
+def download_screenshot():
+    w2if = vtkWindowToImageFilter()
+    w2if.SetInput(renderWindow)
+    w2if.SetInputBufferTypeToRGB()
+    w2if.ReadFrontBufferOff()
+    #
+    writer1 = vtkPNGWriter()
+    writer1.SetInputConnection(w2if.GetOutputPort())
+    writer1.SetWriteToMemory(True)
+    writer1.Write()
+    #
+    return server.protocol.addAttachment(memoryview(writer1.GetResult()))
+
+
+with SinglePageLayout(server) as layout:
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VSlider(
+            v_model=("resolution", DEFAULT_RESOLUTION),
+            min=3,
+            max=60,
+            style="max-width: 300px",
+            dense=True,
+            hide_details=True,
+        )
+        with vuetify.VBtn(
+            icon=True,
+            click="utils.download('vtk.png', trigger('download_screenshot'), 'image/png')",
+        ):
+            vuetify.VIcon("mdi-camera")
+
+    with layout.content:
+        with vuetify.VContainer(classes="pa-0 fill-height", fluid=True):
+            view = vtk.VtkRemoteView(renderWindow)
+            ctrl.view_update = view.update
+
+
+# -----------------------------------------------------------------------------
+# start server
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    server.start()

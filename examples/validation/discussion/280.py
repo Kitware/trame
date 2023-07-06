5c2a424f755224bc62f1c2aef691dcf4cfd12e61
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-07-06 08:30:55 -0600

    docs(discussion): Add sample code

---
+++ examples/validation/discussion/280.py
@@ -0,0 +1,16 @@
+from trame.app import get_server
+from trame.ui.vuetify import VAppLayout
+from trame.widgets import vuetify
+
+server = get_server()
+
+
+@server.state.change("value")
+def update_number(value, **kwargs):
+    print(f"{type(value)}({value})")
+
+
+with VAppLayout(server):
+    vuetify.VTextField(v_model_number=("value", 1), type="number")
+
+server.start()

923c08023668ebc226e4a90706b832dc0d8835f2
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-08-14 20:34:45 -0600

    fix(widgets): add helper to create widget class

---
+++ examples/validation/core/33_outside_lib.py
@@ -0,0 +1,97 @@
+from trame.app import get_server
+from trame.ui.html import DivLayout
+from trame.widgets import vuetify, html, helper
+
+# From: https://quasar.dev/start/umd
+
+module = dict(
+    scripts=[
+        "https://cdn.jsdelivr.net/npm/quasar@2.11.5/dist/quasar.umd.prod.js",
+    ],
+    styles=[
+        "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons",
+        "https://cdn.jsdelivr.net/npm/quasar@2.11.5/dist/quasar.prod.css",
+    ],
+    vue_use=[
+        "Quasar",
+    ],
+)
+
+QSlider = helper.create_class(
+    "QSlider",
+    "q-slider",
+    module=module,
+    properties=[
+        "min",
+        "max",
+    ],
+)
+
+QBtn = helper.create_class(
+    "QBtn",
+    "q-btn",
+    module=module,
+    properties=[
+        "label",
+    ],
+    events=[
+        "click",
+    ],
+)
+
+QCircularProgress = helper.create_class(
+    "QCircularProgress",
+    "q-circular-progress",
+    module=module,
+    properties=[
+        "value",
+        "indeterminate",
+        "size",
+        "thickness",
+        "color",
+        "center_color",
+    ],
+)
+
+# -----------------------------------------------------------------------------
+# Trame usage
+# -----------------------------------------------------------------------------
+
+server = get_server()
+server.client_type = "vue3"
+
+
+def reset():
+    server.state.value = 5
+
+
+with DivLayout(server) as layout:
+    with html.Div(classes="q-pa-md"):
+        with html.Div(classes="row items-center"):
+            html.Div("{{ value }}", classes="col-2")
+            QBtn(label="Hello", classes="col", click=reset)
+            QCircularProgress(
+                indeterminate=True,
+                size="75px",
+                thickness=0.6,
+                color="lime",
+                center_color="grey-8",
+                classes="q-ma-md col",
+            )
+            QSlider(
+                v_model_number=("value", 0),
+                min=("1",),
+                max=("100",),
+                step=("1",),
+                classes="col",
+            )
+            QCircularProgress(
+                size="75px",
+                thickness=0.6,
+                color="lime",
+                center_color="grey-8",
+                classes="q-ma-md col",
+                value=("value",),
+            )
+
+server.start()
---
+++ trame/widgets/helper.py
@@ -0,0 +1,22 @@
+from trame_client.widgets.core import AbstractElement
+
+
+def create_class(class_name, component_name, properties=[], events=[], module=None):
+    """Helper for creating Widget class
+
+    Args:
+        class_name (string): name of the Python generated class
+        component_name (string): name of the vue component
+        properties (list, optional): List of properties mapping. Defaults to [].
+        events (list, optional): List of events mapping. Defaults to [].
+        module (dict, optional): Module to enable when using the class. Defaults to None.
+    """
+
+    def constructor(self, **kwargs):
+        AbstractElement.__init__(self, component_name, **kwargs)
+        if module is not None:
+            self.server.enable_module(module)
+        self._attr_names += properties
+        self._event_names += events
+
+    return type(class_name, (AbstractElement,), dict(__init__=constructor))

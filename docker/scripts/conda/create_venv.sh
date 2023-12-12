ae53161017b20a433a2853b1a21cb50cb9e1823a
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-12-12 11:28:57 -0700

    ci: make docker images for 3.9, 3.10

--- docker/scripts/conda/create_venv.sh
+++ docker/scripts/conda/create_venv.sh
@@ -1,6 +1,6 @@
 # Ensure we are in the base conda environment first, or it won't work.
 . $CONDA_DIR/bin/activate
 
-conda create -y conda python=3.9 --prefix $TRAME_VENV
+conda create -y conda python=$TRAME_PYTHON --prefix $TRAME_VENV
 conda activate $TRAME_VENV
 conda clean -afy

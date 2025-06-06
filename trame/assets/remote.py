351d13b7a44e7606bd0acef01abb6f595e801593
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-06-06 08:40:02 -0600

    fix(assets): expose download_file_from_google_drive

--- trame/assets/remote.py
+++ trame/assets/remote.py
@@ -1,7 +1,13 @@
-from trame_common.assets.remote import AbstractRemoteFile, GoogleDriveFile, HttpFile
+from trame_common.assets.remote import (
+    AbstractRemoteFile,
+    GoogleDriveFile,
+    HttpFile,
+    download_file_from_google_drive,
+)
 
 __all__ = [
     "AbstractRemoteFile",
     "GoogleDriveFile",
     "HttpFile",
+    "download_file_from_google_drive",
 ]

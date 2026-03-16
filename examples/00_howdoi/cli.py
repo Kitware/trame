dad575afc6b5d2e04193a9fdc56841ddb65d6bc6
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-03-15 21:09:16 -0600

    docs(examples): update how to cli

--- examples/00_howdoi/cli.py
+++ examples/00_howdoi/cli.py
@@ -1,13 +1,36 @@
-r"""
-Installation requirements:
-    pip install trame
-"""
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "trame",
+# ]
+# ///
 
-from trame.app import get_server
+from trame.app import TrameApp
+
+
+class AppWithCLI(TrameApp):
+    def __init__(self, server=None):
+        super().__init__(server)
+
+        # Argparse available on server
+        parser = self.server.cli
+        parser.add_argument("-o", "--output", help="Working directory")
+        (args, unknown) = parser.parse_known_args()
+
+        print("args.output", args.output)
+        print("unknown", unknown)
+
+
+def main():
+    app = AppWithCLI()
+    app.server.start(
+        open_browser=False,  # skip browser opening
+        show_connection_info=False,  # no banner
+        timeout=1,  # auto exit after 1s
+    )
+
 
 if __name__ == "__main__":
-    server = get_server()
-    parser = server.cli
-    parser.add_argument("-o", "--output", help="Working directory")
-    (args, _unknown) = parser.parse_known_args()
-    print(args.output)
+    main()

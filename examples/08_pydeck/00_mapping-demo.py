12b1eb024f133116efd025de964dc2a0c32a571b
Ulysse DURAND <ulyssedurand@hotmail.fr>
2026-04-10 14:32:21 +0200

    docs(examples): make panda object serializable

--- examples/08_pydeck/00_mapping-demo.py
+++ examples/08_pydeck/00_mapping-demo.py
@@ -56,7 +56,7 @@ def from_data_file(filename):
         "https://raw.githubusercontent.com/streamlit/"
         "example-data/master/hello/v1/%s" % filename
     )
-    return pd.read_json(url)
+    return pd.read_json(url).to_dict(orient="records")
 
 
 ALL_LAYERS = {

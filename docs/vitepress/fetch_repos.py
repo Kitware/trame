d22651d5229663fdc94d0350cd7fde31e47318de
Ulysse DURAND <ulyssedurand@hotmail.fr>
2026-06-30 11:02:16 +0200

    docs(widgets): add repositories whitelist

--- docs/vitepress/fetch_repos.py
+++ docs/vitepress/fetch_repos.py
@@ -27,30 +27,38 @@ def make_gh_request(request: list[str]):
     return result.stdout
 
 
-def retrieve_gh_repos_from_topic(topic: str):
-    request = f"gh search repos --topic {topic} --limit 1000 --json fullName"
-    result = json.loads(make_gh_request(request.split(" ")))
-    result = [t["fullName"] for t in result]
-    return result
+def retrieve_gh_repos_from_topic(topic: str, owners: list = []):
+    repos = []
+    for owner in owners:
+        request = [
+            "gh",
+            "search",
+            "repos",
+            "--topic",
+            topic,
+            "--owner",
+            owner,
+            "--limit",
+            "1000",
+            "--json",
+            "fullName",
+        ]
+        result = json.loads(make_gh_request(request))
+        for t in result:
+            repos.append(t["fullName"])
+    return repos
 
 
-def retrieve_multiple_repos_graphql(
-    repos: list[str], additional_files: dict[str, str] = {}
-):
-    # Build the query parts
+def retrieve_gh_repos_from_file(filename: str) -> list[str]:
+    with open(filename, "r") as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+def retrieve_multiple_repos_graphql(repos: list[str]):
     repo_queries = []
     for repo in repos:
         owner, name = repo.split("/")
         alias = repo.replace("/", "_").replace("-", "_")
-        files_queries = []
-        for file_key, file_path in additional_files.items():
-            files_queries.append(f"""
-                {file_key}: object(expression: "HEAD:{file_path}") {{
-                    ... on Blob {{
-                        text
-                    }}
-                }}""")
-        files_part = "\n".join(files_queries) if files_queries else ""
         repo_query = f"""
             {alias}: repository(owner: "{owner}", name: "{name}") {{
                 name
@@ -64,7 +72,6 @@ def retrieve_multiple_repos_graphql(
                         }}
                     }}
                 }}
-                {files_part}
             }}"""
         repo_queries.append(repo_query)
     query = f"""
@@ -100,7 +107,12 @@ def repos_to_json(repos, ignored_topics=[]):
 
 
 if __name__ == "__main__":
-    repos = retrieve_gh_repos_from_topic("trame")
+    repos = list(
+        dict.fromkeys(
+            retrieve_gh_repos_from_topic("trame", ["Kitware", "KitwareMedical"])
+            + retrieve_gh_repos_from_file("external_gh_repos.txt")
+        )
+    )
     repos_datas = retrieve_multiple_repos_graphql(repos)
     with open("repos.json", "w") as f:
         json.dump(repos_to_json(repos_datas, ["trame"]), f)
